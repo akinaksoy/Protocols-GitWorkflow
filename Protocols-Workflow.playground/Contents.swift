76f262d66afb6ed1fe71437517289ed82c4cc722
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-08-03 14:42:39 +0300

    project created

---
+++ Protocols-Workflow.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+protocol Car {
+    var name : String { get set }
+    var wheel : Int {get set}
+    var speed : Int {get set}
+    func calculateHours(kilometer : Int) -> Int
+}
+
+class BMW : Car {
+    var speed = 200
+    
+    var name = "BMW"
+    
+    var wheel = 4
+    
+    func calculateHours(kilometer : Int) -> Int {
+        return kilometer/speed
+    }
+}
+
+class Mercedes : Car {
+    var name = "Mercedes"
+    
+    var wheel = 4
+    
+    var speed = 100
+    
+    func calculateHours(kilometer: Int) -> Int {
+        return kilometer/speed
+    }
+    
+    
+}
+
+func returnCarInfo(_ car : Car) {
+    print("\(car.name)'s speed is \(car.speed).Wheel count's is \(car.wheel). The \(car.name) travels 600 kilometers in \(car.calculateHours(kilometer: 600)).")
+}
+
+let mercedes = Mercedes()
+let bmw = BMW()
+
+returnCarInfo(bmw)
+returnCarInfo(mercedes)
